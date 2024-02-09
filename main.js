1ab939a0f9622e0f29bab7ce9fd81849de45a304
Denis Popov <denis@pixeladas.es>
2024-02-09 17:00:49 +0100

    Improving behavior

--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 import './style.css'
 
 let opened = false
+let num = null
 
 const changeLayout = (e) => {
-  const num = e.currentTarget.className.charAt(e.currentTarget.className.length - 1)
   const container = document.querySelector(`.${!opened ? 'grid' : 'grid--changed'}`)
+
+  if (num && e.currentTarget.className.charAt(e.currentTarget.className.length - 1) !== num) {
+    num = e.currentTarget.className.charAt(e.currentTarget.className.length - 1)
+    container.className = `container grid--changed grid--changed__${num}`
+    opened = true
+    return
+  }
+
+  num = e.currentTarget.className.charAt(e.currentTarget.className.length - 1)
   container.className = `container ${opened ? 'grid' : `grid--changed grid--changed__${num}`}`
   opened = !opened
 }
 
 const links = document.querySelectorAll('a')
 const elements = Array.from(links)
-
 
 for (const el of elements) {
   el.addEventListener('click', changeLayout)
